e709587a4882ea9235adf51b295c745f5e5171d6
Valentin Nicolas <valentin.ncls@gmail.com>
2017-03-06 11:48:46 +0100

    import student.json in studentList.js

--- studentsList.js
+++ studentsList.js
@@ -1,1 +1,16 @@
 // Write yout Javascript code in this files
+console.log("test");
+
+var req = new XMLHttpRequest();
+req.open('GET', 'students.json', true);
+req.onreadystatechange = function () {
+  if (req.readyState == 4) {
+     if(req.status == 200) {
+      var students = JSON.parse(req.responseText);
+     console.log(students);
+  }   else {
+      dump("Erreur pendant le chargement de la page.\n");
+    }
+  }
+};
+req.send();
