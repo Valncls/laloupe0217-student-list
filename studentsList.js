e2a59fbf67f13c76ff5733efeb74425773e61a21
Valentin Nicolas <valentin.ncls@gmail.com>
2017-03-28 16:02:01 +0200

    add files

--- studentsList.js
+++ studentsList.js
@@ -1,13 +1,20 @@
 // Write yout Javascript code in this files
-console.log("test");
+
 
 var req = new XMLHttpRequest();
+var table = document.getElementById('students');
 req.open('GET', 'students.json', true);
 req.onreadystatechange = function () {
   if (req.readyState == 4) {
      if(req.status == 200) {
       var students = JSON.parse(req.responseText);
-     console.log(students);
+     students.forEach(function (student) {
+       let tr = document.createElement('tr');
+       tr.innerHTML = `<td>${student.firstname}</td>
+                       <td>${student.lastname}</td>`;
+      table.appendChild(tr);
+       console.log(student.firstname);
+     });
   }   else {
       dump("Erreur pendant le chargement de la page.\n");
     }
